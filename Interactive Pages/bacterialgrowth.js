d5dae817dbc7ac90e9acbaa1e385672ab7e4a9d1
Noah Dirksen <noah05dirksen@gmail.com>
2024-05-04 00:40:36 +0200

    maze gen #1
    bacterial growth

---
+++ Interactive Pages/bacterialgrowth.js
@@ -0,0 +1,156 @@
+
+
+var visited = [];
+const bacterialGrowth = function(){
+    clearInterval(bacterialInterval);
+    visited = [];
+    generateGrid();
+
+    //First choose a random cell to infect:
+    for(let i = 0; i < 15; i++){
+        let x = Math.floor(Math.random() * columns);
+        let y = Math.floor(Math.random() * rows);
+        grid[x][y].color = 'blue';
+
+        visited.push({ x: x, y: y });
+    }
+    drawMaze();
+
+    bacterialGrowthSolve();
+}
+var bacterialInterval;
+const bacterialGrowthSolve = function(){
+    if(timeStepSize == 0){
+        while(visited.length > 0){
+            let index = Math.floor(Math.random() * visited.length);
+            grow(visited[index].x, visited[index].y);
+        }
+        drawMaze();
+        return;
+    }
+    bacterialInterval = setInterval(function() {
+        if(visited.length == 0){
+            clearInterval(bacterialInterval);
+            return;
+        }
+        let index = Math.floor(Math.random() * visited.length);
+        grow(visited[index].x, visited[index].y);
+        drawMaze();
+    }, timeStepSize);
+}
+
+function grow(x, y){
+    let direction;
+    let search = true;
+    if(removeIfEmpty(x, y)) 
+        return false;
+    while(search){
+        direction = Math.floor(Math.random() * 4);
+        switch(direction) {
+            case 0:
+                search = !canGrowUp(x, y);
+                break;
+            case 1:
+                search = !canGrowDown(x, y);
+                break;
+            case 2:
+                search = !canGrowRight(x, y);
+                break;
+            case 3:
+                search = !canGrowLeft(x, y);
+                break;
+        }
+    }
+
+    switch(direction) {
+        case 0:
+            addIfFull(x, y - 1);
+            grid[x][y].top = false;
+            grid[x][y - 1].bottom = false;
+            break;
+        case 1:
+            addIfFull(x, y + 1);
+            grid[x][y].bottom = false;
+            grid[x][y + 1].top = false;
+            break;
+        case 2:
+            addIfFull(x + 1, y);
+            grid[x][y].right = false;
+            grid[x + 1][y].left = false;
+            break;
+        case 3:
+            addIfFull(x - 1, y);
+            grid[x][y].left = false;
+            grid[x - 1][y].right = false;
+            break;
+    }
+    return true;
+}
+
+function addOrRemove(x, y){
+    if(removeIfEmpty(x, y)) return true;
+    if(addIfFull(x, y)) return true;
+    return false;
+}
+function removeIfEmpty(x, y){
+    if(cantGrow(x, y)){
+        //console.log(`remove at ${x}, ${y}`)
+        let indexInVisited = findPositionInVisited(x, y);
+        if(indexInVisited > -1) {
+            visited.splice(indexInVisited, 1);
+            if(grid[x][y].color == 'red')
+                grid[x][y].color = 'orange';
+        }
+        return true;
+    } 
+    return false;
+}
+
+function findPositionInVisited(x, y){
+    for(let i = 0; i < visited.length; i++){
+        if(visited[i].x == x && visited[i].y == y){
+            return i;
+        }
+    }
+    return -1;
+}
+
+function addIfFull(x, y){
+    if(neverVisited(x, y)){
+        visited.push({x: x, y: y});
+        grid[x][y].color = 'red';
+        return true;
+    } 
+    return false;
+}
+function neverVisited(x, y){
+    return grid[x][y].top && grid[x][y].bottom && grid[x][y].left && grid[x][y].right;
+}
+function cantGrow(x, y){
+    return !canGrowUp(x, y) && !canGrowDown(x, y) && !canGrowLeft(x, y) && !canGrowRight(x, y);
+}
+
+function canGrowUp(x, y){
+    if(y > 0){
+        return neverVisited(x, y - 1);
+    }
+    return false;
+}
+function canGrowDown(x, y){
+    if(y < rows - 1){
+        return neverVisited(x, y + 1);
+    }
+    return false;
+}
+function canGrowRight(x, y){
+    if(x < columns - 1){
+        return neverVisited(x + 1, y);
+    }
+    return false;
+}
+function canGrowLeft(x, y){
+    if(x > 0){
+        return neverVisited(x - 1, y);
+    }
+    return false;
+}
--- Interactive Pages/mazegen.js
+++ Interactive Pages/mazegen.js
@@ -2,3 +2,119 @@ console.log("maze gen setup!");
 
 var columns = 10;
 var rows    = 10;
+var timeStepSize = 150;
+
+
+//Retrieve the information we need to draw on the canvas:
+const mazeCanvas = document.getElementById("maze-canvas");
+const mazeCtx = mazeCanvas.getContext('2d');
+
+const mazeColumnsInput    = document.getElementById("maze-columns");
+const mazeColumnsDisplay  = document.getElementById("maze-columns-display");
+
+const mazeRowsInput       = document.getElementById("maze-rows");
+const mazeRowsDisplay    = document.getElementById("maze-rows-display");
+
+const timeStepInput    = document.getElementById("time-step");
+const timeStepDisplay  = document.getElementById("time-step-display");
+
+var cellWidth;
+var cellHeight;
+
+//Make sure we update the display
+updateRowDisplay();
+updateColumnDisplay();
+updateTimeDisplay();
+mazeColumnsInput.addEventListener('input', function() { updateColumnDisplay(); });
+function updateColumnDisplay(event){
+    columns = mazeColumnsInput.value;
+    mazeColumnsDisplay.innerHTML = columns;
+    cellWidth = mazeCanvas.clientWidth / columns;
+    generateGrid();
+}
+mazeRowsInput.addEventListener('input', function() { updateRowDisplay(); });
+function updateRowDisplay(event){
+    rows = mazeRowsInput.value;
+    mazeRowsDisplay.innerHTML = rows;
+    cellHeight = mazeCanvas.clientHeight / rows;
+    generateGrid();
+}
+timeStepInput.addEventListener('input', function() { updateTimeDisplay(); });
+function updateTimeDisplay(event){
+    timeStepSize = timeStepInput.value;
+    timeStepDisplay.innerHTML = timeStepSize;
+}
+timeStepInput.addEventListener('change', function() { 
+    clearInterval(bacterialInterval);
+    bacterialGrowthSolve(); 
+    console.log("changed");
+});
+
+//Now we generate the maze from the given columns and rows:
+//Grid is a 2D array that will keep track of every cell in the maze.
+var grid = [[]];
+generateGrid();
+function generateGrid(){
+    grid = [];
+    for(let x = 0; x < columns; x++){
+        grid[x] = [];
+        for(let y = 0; y < rows; y++){
+            //Every cell should contain information about wether or not it has a wall above it (top), below it (bottom) etc.
+            //And the background color of the cell (color).
+            grid[x][y] = { top: true, bottom: true, left: true, right: true, color: 'green' };
+        }
+    }
+    drawMaze();
+}
+
+drawMaze();
+function drawMaze() {
+    //Clear canvas:
+    mazeCtx.fillStyle = 'white';
+    mazeCtx.fillRect(0, 0, mazeCanvas.clientWidth, mazeCanvas.clientHeight);
+
+    //Now draw each cell:
+    mazeCtx.beginPath()
+    for(let x = 0; x < columns; x++){
+        for(let y = 0; y < rows; y++){
+            //Draw cell background color:
+            mazeCtx.fillStyle = grid[x][y].color;
+            mazeCtx.fillRect(cellWidth * x, cellHeight * y, cellWidth, cellHeight);
+            prepareCell(x, y);
+        }
+    }
+    mazeCtx.closePath();
+    mazeCtx.lineWidth = '3';
+    mazeCtx.stroke();
+}
+
+
+function drawCell(x, y){
+    
+    mazeCtx.beginPath();
+    prepareCell(x, y);
+    mazeCtx.closePath();
+    mazeCtx.lineWidth = '3';
+    mazeCtx.stroke();
+}
+
+function prepareCell(x, y){
+    
+    //Now we draw the walls of the cell, this requires us to check wether or not a wall is present:
+    if(grid[x][y].top){
+        mazeCtx.moveTo(cellWidth * (x + 1), cellHeight * y);
+        mazeCtx.lineTo(cellWidth * x, cellHeight * y);
+    }
+    if(grid[x][y].bottom){
+        mazeCtx.moveTo(cellWidth * (x + 1), cellHeight * (y + 1));
+        mazeCtx.lineTo(cellWidth * x, cellHeight * (y + 1));
+    }
+    if(grid[x][y].right){
+        mazeCtx.moveTo(cellWidth * (x + 1), cellHeight * (y + 1));
+        mazeCtx.lineTo(cellWidth * (x + 1), cellHeight * y);
+    }
+    if(grid[x][y].left){
+        mazeCtx.moveTo(cellWidth * x, cellHeight * (y + 1));
+        mazeCtx.lineTo(cellWidth * x, cellHeight * y);
+    }
+}
